f4904ab4e5637eb46a39e2f57acb6e2349342c28
George Papa <giorgos.papachristou@commsquare.com>
2018-08-23 13:05:54 +0300

    App V1.2
    Extra functionallity - Add models configuration file

--- WebApplication/PythonApp/dataset_handler.py
+++ WebApplication/PythonApp/dataset_handler.py
@@ -3,13 +3,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
 from keras.preprocessing.text import Tokenizer
 
+
 class DatasetSpliter(object):
 
-    def __init__(self):
-        self.data = pd.read_csv('./data/25_cleaned_job_descriptions.csv', header = 0, names = ['Query', 'Description'])
+    def __init__(self,dataset_path,vocab_size,max_length):
+        self.data = pd.read_csv(dataset_path, header = 0, names = ['Query', 'Description'])
         self.split_data()
-        self.vocab_size=500
-        self.max_length=500
+        self.vocab_size=vocab_size
+        self.max_length=max_length
         self.split_data()
 
     def split_data(self):
@@ -24,11 +25,8 @@ class DatasetSpliter(object):
 
     def data_encode(self):
 
-        # parameters
-        vocab_size = 1000
-
         # define Tokenizer with Vocab Size
-        tokenizer = Tokenizer(num_words=vocab_size)
+        tokenizer = Tokenizer(num_words=self.vocab_size)
         tokenizer.fit_on_texts(self.train_descs)
         x_train = tokenizer.texts_to_matrix(self.train_descs, mode='tfidf')
         x_test = tokenizer.texts_to_matrix(self.test_descs, mode='tfidf')
@@ -39,4 +37,3 @@ class DatasetSpliter(object):
         y_test = encoder.transform(self.test_labels)
 
         return x_train, y_train, x_test, y_test
-
