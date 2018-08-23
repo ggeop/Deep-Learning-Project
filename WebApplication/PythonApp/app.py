29ed87840c03288ca3d3212602cd70b41c68a7e9
George Papa <giorgos.papachristou@commsquare.com>
2018-08-23 14:00:36 +0300

    App V1.3
    Extra functionllity - Give the choice to the user to choose to run the application or only the model

--- WebApplication/PythonApp/app.py
+++ WebApplication/PythonApp/app.py
@@ -1,6 +1,10 @@
+import os
 from flask import Flask, flash, redirect, render_template, request, session, abort
 from models.keras_first_go import KerasFirstGoModel
+from clear_bash import clear_bash
+
 app = Flask(__name__)
+cleaner=clear_bash()
 
 def train_model():
     global first_go_model
@@ -23,9 +27,52 @@ def result():
       result = {'Job': processed_text}
       return render_template("result.html",result = result)
 
+def clear_bash():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 if __name__ == "__main__":
+    clear_bash()
+    print("---------------------------------")
+    print("JOB PREDICTION APPLICATION")
+    print("---------------------------------")
+    print("\n\n")
+    print("--- SET MODE ---")
+    print("\n")
+    print("Do you want to start the application or test the model? ")
+    print("\n")
+    print("NOTE: Press 'app' for application of 'model' for model...")
+    mode = input()
 
-    print(("* Loading Keras model and Flask starting server..."
-           "please wait until server has fully started"))
+    if mode=='app':
 
-    app.run()
+        clear_bash()
+
+        print("---------------------------------")
+        print("JOB PREDICTION APPLICATION")
+        print("---------------------------------")
+        print(("*Flask starting server..."
+                   "please wait until server has fully started"))
+        app.run()
+
+    elif mode=='model':
+
+        clear_bash()
+
+        print("---------------------------------")
+        print("PLAY WITH KERAS MODEL")
+        print("---------------------------------")
+
+        print("Wait until I'm ready..")
+        train_model()
+        print("\n\n")
+        print("OK the model is ready!!")
+        skills_description = input("Give me your skill description please: ")
+        prediction = first_go_model.prediction(skills_description)
+        clear_bash()
+        print("This job is my suggetion: ", prediction)
+        print("Thanks for all!!")
+        clear_bash()
+
+    else:
+        print("You are stupid!! BYEEE")
